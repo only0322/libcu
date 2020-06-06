3580d03946762a2d56e66463f5ed0d582d810fa2
吉法师 <765715928@qq.com>
2020-06-06 16:57:25 +0800

    提交

--- date/mydate.h
+++ date/mydate.h
@@ -6,19 +6,17 @@ namespace LibcuDate
     class MyDate
     {
         public:
-            void getNowDateStr(char * today);  //返回YYYYMMDD的日期字符串
-            
+            void getNowDateStr(char * today)  //返回YYYYMMDD的日期字符串
+            {
+                time_t lt;
+                struct tm * local;
+                lt = time(0);
+                local = localtime(&lt);
+                strftime(today,9,"%Y%m%d",local);
+            }
 
 
     };
 
-    inline void MyDate::getNowDateStr(char * today)
-    {
-        time_t lt;
-        struct tm * local;
-        lt = time(0);
-        local = localtime(&lt);
-        strftime(today,9,"%Y%m%d",local);
-    }
 }
 #endif
